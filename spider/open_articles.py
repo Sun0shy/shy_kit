f26a0f6787e92b30e001e2e8427763b6b5612331
Sun0shy <135097288+Sun0shy@users.noreply.github.com>
2024-10-29 10:15:26 +0800

    a

--- spider/open_articles.py
+++ spider/open_articles.py
@@ -1,13 +1,14 @@
 import webbrowser
 
 # 读取txt文件
+# 将文献保存进articles，每一行一条文献
 with open(r"E:\\240821项目跟进\\项目5-免疫相关的肠癌风险预后模型构建及验证-宋荣峰\\articles.txt") as file:
     articles_lists = file.readlines()
-    # print(articles_lists[5])
 
 
-# for article in articles_lists[31:37]:
-#     article = article.strip()
-#     if article:
-#         search_url = f"https://pubmed.ncbi.nlm.nih.gov/?term={article}&filter=pubt.review"
-#         webbrowser.open(search_url)
+# 遍历文献txt，依次打开文献地址
+for article in articles_lists:
+    article = article.strip() 
+    if article:
+        search_url = f"https://pubmed.ncbi.nlm.nih.gov/?term={article}&filter=pubt.review"
+        webbrowser.open(search_url)
